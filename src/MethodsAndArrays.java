7856183f819f1c4458dd9b67089ed4a8369599be
Jason-Pearson <jason-pearson-1-0-7@hotmail.com>
2015-09-22 11:48:43 -0400

    Added Driver Class and Learned Casting

---
+++ src/MethodsAndArrays.java
@@ -0,0 +1,13 @@
+
+public class MethodsAndArrays {//this is your drive class
+
+	public static void main(String[] args) {
+		long myLongVariable = 100000L;//L - tells that the number is a Long -Long is primitive data type - 64 bit - Long ass integer
+		int myIntVariable;
+		//Cast Long to Int
+		myIntVariable = (int)myLongVariable;//cast operator for int - now the Long is an Int
+		
+		System.out.println(myIntVariable);//Display to Check
+	}
+
+}
